c64d9c81cf90e135d67963d89d2dd2c314ee1d2c
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-04-28 16:34:03 -0500

    Se modicaron register y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
@@ -23,12 +17,14 @@
 
             <div>
                 <x-label for="email" value="{{ __('Correo') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                    autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Contraseña') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -40,7 +36,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
                         {{ __('Olvidaste la contraseña?') }}
                     </a>
                 @endif
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,46 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<link rel="shortcut icon" href="images/logo_login.png">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body >
-        <div class="font-sans text-gray-200 antialiased">
-            {{ $slot }}
-        </div>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    </body>
-        <style>
-body{
-    background-color: blue;
-    
-}
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-.sm\:pt-0 {
-    padding-top: 0px;
-    background-color:#27292a;
-    background-image: url('images/wallpaper_login.jpg');
-    width: 100%;
-    height: auto;
-    background-size: cover;
-    background-position: center;
-}
-.w-16 {
-    
-}
+<body>
+    <div class="font-sans text-gray-200 antialiased">
+        {{ $slot }}
+    </div>
 
 
-        </style>
+</body>
+<style>
+    body {
+        background-color: blue;
+
+    }
+
+    .sm\:pt-0 {
+        padding-top: 0px;
+        background-color: #27292a;
+        background-image: url('images/wallpaper_login.jpg');
+        width: 100%;
+        height: auto;
+        background-size: cover;
+        background-position: center;
+    }
+
+    .w-16 {}
+</style>
 
 </html>
