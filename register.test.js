028e01e3747d77ac0da5e2755dbac7e3b66aceb4
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-04-28 17:48:55 -0500

    Se agregaron más pruebas

--- register.test.js
+++ register.test.js
@@ -2,4 +2,8 @@ const register = require('./register');
 
 test('verifies credentials for register', () => {
   expect(register('SuperAdmin', 'admin@technoblog.com', 'ornitorincoBailando751?')).toBe(true);
+  expect(register('CoderFrontEnd', 'admin@technoblog.com', '123')).toBe(false);
+  expect(register('Cod', 'admin@technoblog.com', '123')).toBe(false);
+  expect(register('JesusAlcalino', 'jesus@gmail.com', 'alcalino787Jesus')).toBe(true);
+  expect(register('BrosMario', 'mario@nintendo.com', 'NintendoMario1928')).toBe(true);
 });
