e73fceed5fd546894645e603880e687d586725af
Sergio Escudero Tabares <sergioset15@gmail.com>
2023-05-18 16:53:07 -0500

    Prueba de usuario

--- tests/Unit/InicioSesionTest.php
+++ tests/Unit/InicioSesionTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Dotenv\Validator;
 use PHPUnit\Framework\TestCase;
 use App\Models\User;
+use App\Models\Usuario;
 
 class InicioSesionTest extends TestCase
 {
@@ -15,9 +16,11 @@ class InicioSesionTest extends TestCase
     public function test_comprobar_email()
     {
         $usuario = new User(['Sergio Escudero', 'sergioset7@gmail.com', 'Ornitorrinco2831!']);
+        $usuarioprueba = new Usuario(20, 'Sergio Escudero');
         $correo = 'sergioset7@gmail.com';
-                
-        $this->assertContains('@', str_split($correo), 'Se necesita un @ en el email');
+        
+        $this->assertSame(20, $usuarioprueba->tellAge());
+        // $this->assertContains('@', $usuario[2], 'Se necesita un @ en el email');
     }
         
 }
