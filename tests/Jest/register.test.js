08b99884a37e1a8b57c13dffd38972f176286269
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-05-19 01:39:42 -0500

    Organización de tests npm

--- tests/Jest/register.test.js
+++ tests/Jest/register.test.js
@@ -2,8 +2,20 @@ const register = require('./register');
 
 test('verifies credentials for register', () => {
   expect(register('SuperAdmin', 'admin@technoblog.com', 'ornitorincoBailando751?')).toBe(true);
+});
+
+test('verifies credentials for register', () => {
   expect(register('CoderFrontEnd', 'admin@technoblog.com', '123')).toBe(false);
+});
+
+test('verifies credentials for register', () => {
   expect(register('Cod', 'admin@technoblog.com', '123')).toBe(false);
+});
+
+test('verifies credentials for register', () => {
   expect(register('JesusAlcalino', 'jesus@gmail.com', 'alcalino787Jesus')).toBe(true);
+});
+
+test('verifies credentials for register', () => {
   expect(register('BrosMario', 'mario@nintendo.com', 'NintendoMario1928')).toBe(true);
 });
